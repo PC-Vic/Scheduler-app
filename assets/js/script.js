2e3d16bc5428c7c42db8674a31620be409b4c076
Victor Nieves <c_cc_vic@yahoo.com>
2023-07-13 17:55:14 -0400

    updated file path to script and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,12 +17,7 @@ var hour17Div = document.getElementById('hour-17')
 // in the html.
 $(function () {
   
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+
   saveButtons.forEach(function(button) {
     button.addEventListener('click', function() {
       var timeBlockId = button.parentElement.id;
@@ -50,8 +45,7 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
 
 
 
